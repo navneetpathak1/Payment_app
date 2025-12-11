ba91b2c680b07a4f6c6d3ddd80e1eebfa0d200e5
navneetpathak1 <navneetpathak1909@gmail.com>
2025-12-11 20:34:28 +0530

    new

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const zod = require('zod');
 const jwt = require('jsonwebtoken');
 const { User, Account } = require('../db');
-const { JWT_SECRET } = require('../config');
+const JWT_SECRET = process.env.JWT_SECRET;
 const { authMiddleware } = require("../middleware");
 
 const router = express.Router();
